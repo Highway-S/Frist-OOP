e8873909bd02d536d616badad4d2bec3bad1830c
Highway-S <phoorin.w@kkumail.com>
2024-11-26 11:41:10 +0700

    Update

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Phoorin Wongwanlop";
+string student_id = "673450199-6";
+// string id Datatype ---> ชนิดข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My Student ID : " + student_id);
